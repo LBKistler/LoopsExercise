10a6fb6b2945bfe0290121044018458c6d1907b9
“lbkistler” <loribethkistler@gmail.com>
2022-03-16 08:54:33 -0500

    uploaded without bonus

---
+++ script.js
@@ -0,0 +1,80 @@
+// 1
+for (i = 1; i < 8; i++){
+    console.log(i);
+  };
+
+console.log('======================');
+
+// 2
+for (i = 5; i <= 25; i += 4){
+    console.log(i);
+}
+
+console.log('======================');
+
+// 3a
+const wizards= [
+    `Harry Potter`, 
+    `Hermione Granger`, 
+    `Ron Weasley`
+];
+
+// 3b
+for (names of wizards){
+    console.log(names);
+  }
+
+  /* if you wanted to do this as a "for" loop, you could do the following:
+  for (i = 0; i < wizards.length; i++){
+    console.log(wizards(i));
+  }
+  */
+
+console.log('======================');
+
+// 4a
+let harryPotterMovies = 0;
+
+// 4b & 4c
+while (harryPotterMovies <=8) {
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+};
+
+
+console.log('======================');
+
+// BONUS
+// 5a
+const hogwartsHouses = [
+    `Gryffindor`, 
+    `Hufflepuff`, 
+    `Ravenclaw`, 
+    `Slytherin`
+];
+
+// 5b
+for (category of hogwartsHouses.slice(0,1)){
+   /* for (?? of category){
+      console.log(??);
+    }*/
+  } 
+
+  console.log('======================');
+
+// 6a
+const quote = [
+    `Yer`,
+    `A`,
+    `Wizard`,
+    `Harry`
+];
+
+
+
+// 6b
+
+
+// 7
+
+
